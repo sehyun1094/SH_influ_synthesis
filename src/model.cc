265e13b90574f05cc4bfdc05928df056af498877
Park Sehyun <ps_hyen@snu.ac.kr>
2024-04-03 14:46:02 +0900

    수정

--- src/model.cc
+++ src/model.cc
@@ -172,7 +172,8 @@ namespace flu
         namespace bt = boost::posix_time;
 
         double h_step = dt.hours()/24.0;
-        std::string start_time_str = boost::posix_time::to_simple_string(start_time)
+        
+        std::string start_time_str = boost::posix_time::to_simple_string(start_time);
         printf("Start time: %s\n", start_time_str.c_str());
         const size_t nag = transmission_regular.cols();
         Eigen::VectorXd results = Eigen::VectorXd::Zero(nag*3);
