54c2dc82179489e7817f9d85a6869a1c77181e61
Park Sehyun <ps_hyen@snu.ac.kr>
2024-12-16 12:43:03 +0900

    2024-12-15 수정

--- src/mcmc.h
+++ src/mcmc.h
@@ -49,17 +49,17 @@ mcmc_result_t adaptiveMCMCWithProposal( const Func1 &lprior, const Func2 &llikel
     while(sampleCount<nbatch)
     {
         
-        // printf("\r%d/%d [", sampleCount, nbatch);
-        // percent = (float)sampleCount/MAX*100; 
-        // bar_count = percent/tick; 
-        // for(iii=0; iii<LEN; iii++) { 
-		// 	if(bar_count > iii) { 
-		// 		printf("%c", bar);
-		// 	} else { 
-		// 		printf("%c", blank);
-		// 	}
-		// }
-		// printf("] %0.2f%%", percent); 
+        printf("\r%d/%d [", sampleCount, nbatch);
+        percent = (float)sampleCount/nbatch*100; 
+        bar_count = percent/tick; 
+        for(iii=0; iii<LEN; iii++) { 
+			if(bar_count > iii) { 
+				printf("%c", bar);
+			} else { 
+				printf("%c", blank);
+			}
+		}
+		printf("] %0.2f%%", percent); 
 
         
         ++k;
